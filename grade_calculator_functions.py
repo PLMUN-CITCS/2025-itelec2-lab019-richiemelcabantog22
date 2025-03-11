94a0d482d29af86a6caf4e7f0a941e837a658169
richiemelcabantog22 <>
2025-03-11 23:35:05 +0800

    Submitting Python Week 05 - Laboratory # 19

--- grade_calculator_functions.py
+++ grade_calculator_functions.py
@@ -1,18 +1,22 @@
 def get_student_score():
-    student_score = float(input("Enter your score: "))
-    return student_score
+    score = float(input("Enter your score: "))
+    return score
 
-def calculate_grade(student_score):
-    if student_score >= 90:
+def calculate_grade(score):
+    if score >= 90:
         return 'A'
-    if student_score >= 80:
+    if score >= 80:
         return 'B'
-    if student_score >= 70:
+    if score >= 70:
         return 'C'
-    if student_score >= 60:
+    if score >= 60:
         return 'D'
     return 'F'
 
-student_score = get_student_score()
-grade = calculate_grade(student_score)
-print(f"Your Grade is: {grade}")
+def main():
+    score = get_student_score()
+    grade = calculate_grade(score)
+    print(f"Your Grade is: {grade}")
+
+if __name__ == "__main__":
+    main()
